359d6bc1245414b82dfd3c4223d39bd403765ff6
Gilli (Michael Gilliland) <gillilandmj@upmc.edu>
2014-09-24 14:30:06 -0400

    Removed extraneous whitespace

--- mi_snake.py
+++ mi_snake.py
@@ -47,10 +47,10 @@ if __name__ == '__main__':
     snake = []	        # The snake which is a list of locations occupied
     direction = list(Vector(0, 1))
     points = 0	        # The number of eaten objects
-    key = 0 	        # Holds the pressed key 
+    key = 0 	        # Holds the pressed key
     delay = 100
     key_to_vector = {65:[-1,0], 66:[1,0], 68:[0,-1], 67:[0,1]}
-                                                                                         
+
     # Make a new screen object
     screen = curses.initscr()
     curses.start_color()
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     curses.noecho()
     # Clear and refresh the screen
     screen.clear()
-	
+
     # Give the snake it's first node at the middle of the screen
     snake.append([screen_width / 2, screen_length / 2])
 
@@ -97,29 +97,29 @@ if __name__ == '__main__':
             food_location = generate_food_location(snake, screen_width, screen_length)
 
         screen.clear()
-	
+
         # If the snake hits a wall
         if snake[0][0] >= screen_width or snake[0][1] >= screen_length or snake[0][0] < 0 or snake[0][1] < 0:
             break
-	
+
         # If the snake hits itself
         for i in range(1, len(snake)):
             if snake[0] == snake[i]:
                 key = 101
-	
+
         # Adds characters to view
         for i in range(len(snake)):
             screen.addch(snake[i][0], snake[i][1], "X", curses.color_pair(1))
-		
-        screen.addch(food_location[0], food_location[1], "O", curses.color_pair(2))	 
+
+        screen.addch(food_location[0], food_location[1], "O", curses.color_pair(2))
         screen.move(0, 0)
         screen.refresh()
-	
+
         # If we are going left or right
         if direction[1] != 0:
             curses.napms(delay / 2)
         else:
-            curses.napms(delay)       
+            curses.napms(delay)
 
-	
+
     curses.endwin()
